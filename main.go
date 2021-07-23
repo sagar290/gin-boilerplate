a4858ad5b796cee49214e577e066db1d17c05b99
sagar.dash290 <sagar.dash290@gmail.com>
2021-07-23 21:21:29 +0600

    Update main.go

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"fmt"
 
-	"Controller"
+	"gin-boilerplate/Controller"
 
+	"github.com/foolin/gin-template"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,7 +14,17 @@ func main() {
 	fmt.Println("Hello world")
 
 	r := gin.Default()
-	r.GET("/HomePage", Controller.HomePage)
+	//new template engine
+	//new template engine
+	r.HTMLRender = gintemplate.New(gintemplate.TemplateConfig{
+		Root:         "views",
+		Extension:    ".html",
+		Master:       "index",
+		Partials:     []string{"partials/ad"},
+		DisableCache: true,
+	})
+
+	r.GET("/", Controller.HomePage)
 	r.POST("/PostHomePage", Controller.PostHomePage)
 	r.GET("/query", Controller.QueryString)
 	r.GET("/param/:name/:age", Controller.ParamString)
